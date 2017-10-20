f7207263fb81cf04d0c05a02f502ae360bb08ca4
Justin <justin.zy.wang@gmail.com>
2017-10-20 22:04:46 +0800

    add swarm script

---
+++ install-swarm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Fetch the variables
+. parm.txt
+
+# function to get the current time formatted
+currentTime()
+{
+  date +"%Y-%m-%d %H:%M:%S";
+}
+
+echo ---$(currentTime)---initiate swarm---
+sudo docker swarm init --advertise-addr $LEAD_HOST_IP
+
+sudo docker swarm join-token manager
+#其他manager节点加入，用sudo docker swarm join-token manager来获取加入集群的命令
+
+#如果由于managernode ip改变或其他原因导致集群失效，可以利用sudo docker swarm init --force-new-cluster来重置整个集群
